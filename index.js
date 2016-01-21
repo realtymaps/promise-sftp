4d7c1b4e5ab9cff31c29ee34b3190df22ca51498
Joe Ibershoff <joe@ibershoff.com>
2016-01-21 12:12:35 -0500

    remove coffeescript dependency

--- index.js
+++ index.js
@@ -3,11 +3,8 @@
 'use strict';
 
 
-var coffee = require('coffee-script');
-coffee.register();
-
-module.exports = require('./lib/promiseSftp');
+module.exports = require('./dist/promiseSftp');
 module.exports.FtpConnectionError = require('promise-ftp-common').FtpConnectionError;
 module.exports.FtpReconnectError = require('promise-ftp-common').FtpReconnectError;
 module.exports.STATUSES = require('promise-ftp-common').STATUSES;
-module.exports.ERROR_CODES = require('./lib/errorCodes');
+module.exports.ERROR_CODES = require('./dist/errorCodes');
